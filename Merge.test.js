c8eb64db0eb9604ef282094e39c7f5ca05d8c02c
Juan David <juan.getial@correounivalle.edu.co>
2023-06-24 22:15:54 -0500

    FinalizandoTests

--- Merge.test.js
+++ Merge.test.js
@@ -1,27 +1,27 @@
 const mergeSort = require('./MergeSort.js');
 
 describe('Merge Sort', () => {
-  test('Sorts an unsorted array', () => {
-    const arr = [4, 2, 7, 1, 9];
-    const sortedArr = mergeSort(arr);
+  test('Ordena una matriz sin ordenar', () => {
+    const arreglo = [4, 2, 7, 1, 9];
+    const sortedArr = mergeSort(arreglo);
     expect(sortedArr).toEqual([1, 2, 4, 7, 9]);
   });
 
-  test('Keeps an already sorted array intact', () => {
-    const arr = [1, 2, 4, 7, 9];
-    const sortedArr = mergeSort(arr);
+  test('Mantiene intacta una matriz ya ordenada', () => {
+    const arreglo = [1, 2, 4, 7, 9];
+    const sortedArr = mergeSort(arreglo);
     expect(sortedArr).toEqual([1, 2, 4, 7, 9]);
   });
 
-  test('Handles an empty array', () => {
-    const arr = [];
-    const sortedArr = mergeSort(arr);
+  test('Maneja una matriz vacía', () => {
+    const arreglo = [];
+    const sortedArr = mergeSort(arreglo);
     expect(sortedArr).toEqual([]);
   });
 
-  test('Sorts a reverse ordered array', () => {
-    const arr = [9, 7, 4, 2, 1];
-    const sortedArr = mergeSort(arr);
+  test('Ordena una matriz ordenada inversa', () => {
+    const arreglo = [9, 7, 4, 2, 1];
+    const sortedArr = mergeSort(arreglo);
     expect(sortedArr).toEqual([1, 2, 4, 7, 9]);
   });
 });
--- Quick.test.js
+++ Quick.test.js
@@ -1,27 +1,27 @@
 const quickSort = require('./QuickSort');
 
 describe('Quick Sort', () => {
-  test('Sorts an unsorted array', () => {
-    const arr = [4, 2, 7, 1, 9];
-    quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([1, 2, 4, 7, 9]);
+  test('Ordena una matriz sin ordenar', () => {
+    const arreglo = [4, 2, 7, 1, 9];
+    quickSort(arreglo, 0, arreglo.length - 1);
+    expect(arreglo).toEqual([1, 2, 4, 7, 9]);
   });
 
-  test('Keeps an already sorted array intact', () => {
-    const arr = [1, 2, 4, 7, 9];
-    quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([1, 2, 4, 7, 9]);
+  test('Mantiene intacta una matriz ya ordenada', () => {
+    const arreglo = [1, 2, 4, 7, 9];
+    quickSort(arreglo, 0, arreglo.length - 1);
+    expect(arreglo).toEqual([1, 2, 4, 7, 9]);
   });
 
-  test('Handles an empty array', () => {
-    const arr = [];
-    quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([]);
+  test('Maneja una matriz vacía', () => {
+    const arreglo = [];
+    quickSort(arreglo, 0, arreglo.length - 1);
+    expect(arreglo).toEqual([]);
   });
 
-  test('Sorts a reverse ordered array', () => {
-    const arr = [9, 7, 4, 2, 1];
-    quickSort(arr, 0, arr.length - 1);
-    expect(arr).toEqual([1, 2, 4, 7, 9]);
+  test('Ordena una matriz ordenada inversa', () => {
+    const arreglo = [9, 7, 4, 2, 1];
+    quickSort(arreglo, 0, arreglo.length - 1);
+    expect(arreglo).toEqual([1, 2, 4, 7, 9]);
   });
 });
